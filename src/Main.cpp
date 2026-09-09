172d86fc9d04e993dcb2d02e932f92a7d47d2787
mirkosal <153204213+mirkosal@users.noreply.github.com>
2026-09-09 16:11:02 +0200

    port: migrate plugin entry points to SKSE CommonLib API

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,79 +1,89 @@
 #include "FuzRoDohInternals.h"
 #include "Hooks.h"
-#include "VersionInfo.h"
-#include <ShlObj.h>
 
-
-extern "C"
+namespace
 {
-	void MessageHandler(SKSEMessagingInterface::Message * msg)
-	{
-		switch (msg->type)
-		{
-		case SKSEMessagingInterface::kMessage_InputLoaded:
-			{
-				// schedule a cleanup thread for the subtitle hasher
-				std::thread CleanupThread([]() {
-					while (true)
-					{
-						std::this_thread::sleep_for(std::chrono::seconds(2));
-						SubtitleHasher::Instance.Tick();
-					}
-				});
-				CleanupThread.detach();
+    void InitializeLog()
+    {
+        auto path = logger::log_directory();
+        if (!path) {
+            return;
+        }
 
-				_MESSAGE("Scheduled cleanup thread");
-				_MESSAGE("%s Initialized!", MakeSillyName().c_str());
-			}
-			break;
-		}
-	}
+        *path /= "Fuz Ro D'oh.log";
+        auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+        auto log = std::make_shared<spdlog::logger>("global", std::move(sink));
+        log->set_level(spdlog::level::info);
+        log->flush_on(spdlog::level::info);
+        spdlog::set_default_logger(std::move(log));
+        spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+    }
 
-	__declspec(dllexport) bool SKSEPlugin_Load(const SKSEInterface * skse)
-	{
-		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\Fuz Ro D-oh.log");
+    void MessageHandler(SKSE::MessagingInterface::Message* a_message)
+    {
+        if (!a_message || a_message->type != SKSE::MessagingInterface::kInputLoaded) {
+            return;
+        }
 
-		_MESSAGE("%s Initializing...", MakeSillyName().c_str());
+        static std::once_flag cleanupThreadOnce;
+        std::call_once(cleanupThreadOnce, []() {
+            std::thread([]() {
+                for (;;) {
+                    std::this_thread::sleep_for(std::chrono::seconds(2));
+                    SubtitleHasher::Instance.Tick();
+                }
+            }).detach();
+            logger::info("Scheduled subtitle hash cleanup thread");
+        });
+    }
+}
 
-		interfaces::kPluginHandle = skse->GetPluginHandle();
-		interfaces::kMsgInterface = (SKSEMessagingInterface*)skse->QueryInterface(kInterface_Messaging);
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+    SKSE::PluginVersionData v;
+    v.PluginVersion({ Version::MAJOR, Version::MINOR, Version::PATCH });
+    v.PluginName("Fuz Ro D'oh");
+    v.AuthorName("shadeMe; Skyrim 1.7.104 CommonLib port");
+    v.UsesAddressLibrary();
+    v.CompatibleVersions({ REL::Version{ 1, 7, 104, 0 } });
+    return v;
+}();
 
-		if (!interfaces::kMsgInterface)
-		{
-			_MESSAGE("Couldn't initialize messaging interface");
-			return false;
-		}
-		else if (interfaces::kMsgInterface->interfaceVersion < 2)
-		{
-			_MESSAGE("Messaging interface too old (%d expected %d)", interfaces::kMsgInterface->interfaceVersion, 2);
-			return false;
-		}
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+    InitializeLog();
 
-		_MESSAGE("Initializing INI Manager");
-		FuzRoDohINIManager::Instance.Initialize("Data\\SKSE\\Plugins\\Fuz Ro D'oh.ini", nullptr);
+    if (!a_skse) {
+        return false;
+    }
 
-		if (interfaces::kMsgInterface->RegisterListener(interfaces::kPluginHandle, "SKSE", MessageHandler) == false)
-		{
-			_MESSAGE("Couldn't register message listener");
-			return false;
-		}
-		else if (InstallHooks() == false)
-			return false;
+    const REL::Version targetRuntime{ 1, 7, 104, 0 };
+    if (a_skse->RuntimeVersion() != targetRuntime) {
+        logger::critical("Unsupported Skyrim runtime {}; this test build only supports {}",
+            a_skse->RuntimeVersion().string(), targetRuntime.string());
+        return false;
+    }
 
-		return true;
-	}
+    SKSE::Init(a_skse, false);
 
-	__declspec(dllexport) SKSEPluginVersionData SKSEPlugin_Version =
-	{
-		SKSEPluginVersionData::kVersion,
+    logger::info("Fuz Ro D'oh {} loading on Skyrim {}", Version::NAME, targetRuntime.string());
+    GetFuzSettings().Load();
 
-		PACKED_SME_VERSION,
-		"Fuz Ro D'oh",
-		"shadeMe",
-		"",
-		0,	// Version-dependent
-		0,
-		{ RUNTIME_VERSION_1_6_640, 0 },
-		0,
-	};
-};
+    const auto* messaging = SKSE::GetMessagingInterface();
+    if (!messaging || messaging->Version() < SKSE::MessagingInterface::kVersion) {
+        logger::critical("SKSE messaging interface is unavailable or too old");
+        return false;
+    }
+
+    if (!InstallHooks()) {
+        logger::critical("Hook validation/installation failed; refusing to load the plugin");
+        return false;
+    }
+
+    if (!messaging->RegisterListener(MessageHandler)) {
+        logger::critical("Could not register the SKSE message listener");
+        return false;
+    }
+
+    logger::info("Fuz Ro D'oh initialized successfully");
+    return true;
+}
